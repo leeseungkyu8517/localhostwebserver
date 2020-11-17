c83559e7fdb85e0dcf05243f091e85655a2aa3f5
leeseungkyu8517 <lsg8517@gmail.com>
2020-11-17 09:41:19 +0900

    plus graph

--- webserver.py
+++ webserver.py
@@ -1,4 +1,13 @@
 # WebServer.py
+import serial
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import numpy as np
+
+
+
+
+
 
 from socket import *
 
@@ -36,3 +45,30 @@ while True:
         headerBytes = bytes(header, "UTF-8")
         connectionSocket.send(headerBytes)
         connectionSocket.close()
+
+
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, 50), ylim=(0, 30))
+line, = ax.plot([], [], lw=2)
+
+max_points = 50
+line, = ax.plot(np.arange(max_points),
+                np.ones(max_points, dtype=np.float)*np.nan, lw=2)
+
+def init():
+    return line,
+
+def animate(i):
+    y = connectionSocket.readline()
+    y = y.decode()[:-2]
+    y = float(y)
+
+    old_y = line.get_ydata()
+    new_y = np.r_[old_y[1:], y]
+    line.set_ydata(new_y)
+    return line,
+
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=False)
+
+plt.show()
