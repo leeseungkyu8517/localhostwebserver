2d1c890382eb52e4d0f1ce40a569836441679c46
leeseungkyu <lsg8517@naver.com>
2020-11-17 17:59:14 +0900

    make graph into the webserver

--- webserver.py
+++ webserver.py
@@ -1,8 +1,9 @@
 # WebServer.py
-import serial
+
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import numpy as np
+
 
 
 
@@ -17,6 +18,18 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((serverAddress, serverPort))
 serverSocket.listen(1000)   # 1000 = number of unexpected connection
 print("Ready to server")
+
+countad=0
+countnum=0
+fig = plt.figure()  # figure(도표) 생성
+
+ax = plt.subplot(211, xlim=(0, 50), ylim=(0, 1024))
+
+max_points = 50
+
+line, = ax.plot(np.arange(max_points),
+                np.ones(max_points, dtype=np.float) * np.nan, lw=1, c='blue', ms=1)
+countnum = countnum + countad
 
 
 while True:
@@ -35,40 +48,43 @@ while True:
         headerBytes = bytes(header, "UTF-8")
         connectionSocket.send(headerBytes)
         # when buf size is 1 can't parse html code
-        for i in range(0, len(outputData), 1024):   # send buf size 1024
-            connectionSocket.send(outputData[i:i + 1024])
+        for i in range(0, len(outputData), 8192):   # send buf size 1024
+            connectionSocket.send(outputData[i:i + 8192])
         connectionSocket.send(b"\r\n\r\n")
+        countad = countad + 1
+        print(countad)
+
         connectionSocket.close()
+
     except IOError:
         # 404 Not Found
         header = "HTTP/1.1 404 Not found\r\n"
         headerBytes = bytes(header, "UTF-8")
         connectionSocket.send(headerBytes)
+        countad=countad+1
+        print(countad)
+
+
+        def init():
+            return line
+
+
+        def animate(i):
+            y = countnum
+            old_y = line.get_ydata()
+            new_y = np.r_[old_y[1:], y]
+            line.set_ydata(new_y)
+            print(new_y)
+            return line
+
+
+        anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=50, blit=False)
+
+        plt.show()
+
         connectionSocket.close()
 
 
 
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 50), ylim=(0, 30))
-line, = ax.plot([], [], lw=2)
 
-max_points = 50
-line, = ax.plot(np.arange(max_points),
-                np.ones(max_points, dtype=np.float)*np.nan, lw=2)
 
-def init():
-    return line,
-
-def animate(i):
-    y = connectionSocket.readline()
-    y = y.decode()[:-2]
-    y = float(y)
-
-    old_y = line.get_ydata()
-    new_y = np.r_[old_y[1:], y]
-    line.set_ydata(new_y)
-    return line,
-
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=False)
-
-plt.show()
